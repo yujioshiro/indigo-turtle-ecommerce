f493f65015de72b51258a770c5515482a030aa6d
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-04-24 22:46:46 -0700

    Update docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Indigo Turtle Ecommerce',
-  tagline: 'Let\'s goooooooo!',
+  title: 'IndigoList',
+  tagline: 'Ecommerce site for Hack Weekly - Indigo Turtle',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
