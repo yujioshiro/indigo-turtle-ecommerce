19da204cd612774ef12e6c67beaaf9ff52685da8
Edmund-Frio <edmundfrio@gmail.com>
2023-04-26 09:29:28 -1000

    added navbar and routing

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,29 @@
-import React from "react"
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { NavbarData } from './NavbarData';
+import TurtleLogo from './TurtleLogo.png';
 
-export default function Navbar(): JSX.Element {
+export function Navbar(): JSX.Element {
   return (
-    <div></div>
-  )
+    <div className="p-2.5">
+      <nav className="grid grid-flow-col">
+        <div>
+          <span>
+            <NavLink to={'/HomePage'}>
+              <img src={TurtleLogo} alt="Turtle Logo" />
+            </NavLink>
+          </span>
+        </div>
+        <div>
+          {NavbarData.map((link, index) => {
+            return (
+              <div key={index}>
+                <NavLink to={link.path}>{link.name}</NavLink>
+              </div>
+            );
+          })}
+        </div>
+      </nav>
+    </div>
+  );
 }
