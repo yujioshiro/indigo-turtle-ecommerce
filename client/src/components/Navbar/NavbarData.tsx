0c01d2d98ea7f1aecab5a30992d28fcfd0ea505b
Edmund-Frio <edmundfrio@gmail.com>
2023-04-28 21:12:02 -1000

    made changes to navbar and made global.d.ts file to store types (e.g. added Products type

--- client/src/components/Navbar/NavbarData.tsx
+++ client/src/components/Navbar/NavbarData.tsx
@@ -1,16 +1,43 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { HomePageLink, ProductPageLink, AboutPageLink, CheckoutPageLink, AuthPageLink } from '../../global'
 
-export const NavbarData = [
-  {
-    name: "Sell Product",
-    path: "/ProductPage",
-  },
-  {
-    name: "About Us",
-    path: "/AboutPage",
-  },
-  {
-    name: "Account",
-    path: "/AuthPage",
-  }
-]
+export const Home = (props: HomePageLink) => {
+  return (
+    <NavLink to={props.path}>
+      {props.image}
+    </NavLink>
+  )
+}
+export const SellProduct = (props: ProductPageLink) => {
+  return (
+    <NavLink to={props.path}>
+      {props.name}
+    </NavLink>
+  )
+}
+export const AboutUs = (props: AboutPageLink) => {
+  return (
+    <NavLink to={props.path}>
+      {props.name}
+    </NavLink>
+  )
+}
+export const Checkout = (props: CheckoutPageLink) => {
+  return (
+    <NavLink to={props.path}>
+      {props.icon}
+    </NavLink>
+  )
+}
+export const Account = (props: AuthPageLink) => {
+  return (
+    <NavLink to={props.path}>
+      {props.user}
+    </NavLink>
+  )
+}
+
