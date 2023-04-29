c1fcfdeae2e898f6d2a765ecd8c8d63aed1563f0
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-04-29 02:56:20 +0100

    Home page started

--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function AuthPage(): JSX.Element {
   return <div>AuthPage</div>;
 }
--- client/src/pages/ProductPage/ProductPage.tsx
+++ client/src/pages/ProductPage/ProductPage.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function ProductPage(): JSX.Element {
   return <div>ProductPage</div>;
 }
