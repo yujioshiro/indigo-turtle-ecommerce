a43a40cb775a30f233d571532f30002a529a447f
robertsonj48 <robertsonj48@gmail.com>
2023-05-01 20:24:00 -0400

    added basic about us page with some tailwind

--- client/src/pages/AboutPage/AboutPage.tsx
+++ client/src/pages/AboutPage/AboutPage.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import './AboutPage.css';
 
 export default function AboutPage(): JSX.Element {
-  return <div>AboutPage</div>;
+  return (
+    <div className="flex flex-col my-10 gap-4 content-center">
+      <h1 className="text-neutral-white text-5xl text-center my-10">
+        Welcome to the Indigo Turtle Group!
+      </h1>
+      <div className="text-link text-3xl text-center mx-auto">
+        Meet the crew!
+      </div>
+      <ul className="text-neutral-white text-3xl text-center">
+        <li>DekoMoon</li>
+        <li>Jennifer</li>
+        <li>missbabelfish</li>
+        <li>obi</li>
+        <li>shankssc</li>
+        <li>Shohei</li>
+        <li>solarized7124</li>
+        <li>Yuji</li>
+      </ul>
+      <a
+        className="text-3xl text-link text-center my-10 block"
+        href="https://github.com/Hack-Weekly/indigo-turtle-ecommerce"
+      >
+        Check us out on Github!
+      </a>
+    </div>
+  );
 }
