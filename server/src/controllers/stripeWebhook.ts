04d959da5c628199e520293890ff09ebc5d150eb
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-05-05 17:32:17 -0400

    prisma create Order and OrderItems done

--- server/src/controllers/stripeWebhook.ts
+++ server/src/controllers/stripeWebhook.ts
@@ -19,6 +19,8 @@ type TransactionInstance = Omit<
   '$connect' | '$disconnect' | '$on' | '$transaction' | '$use'
 >;
 
+type UnhandledError = never;
+
 interface Item {
   name: string;
   quantity: number;
@@ -28,6 +30,7 @@ interface Item {
 interface SuccessObject {
   id: string;
   amount_total: number;
+  payment_status: string;
   metadata: {
     userId: number;
   };
@@ -36,7 +39,7 @@ interface SuccessObject {
 const handleDBTransactions = async (
   lineItems: Item[],
   successObject: SuccessObject
-): Promise<void | never> => {
+): Promise<void | UnhandledError> => {
   await prisma.$transaction(async (tx: TransactionInstance) => {
     const products = await Promise.all(
       lineItems.map(
@@ -57,18 +60,8 @@ const handleDBTransactions = async (
 
     const order = await execSideEffectWithError(tx.order.create, {
       data: {
-        userId: successObject.metadata.userId,
+        userId: Number(successObject.metadata.userId),
         total: successObject.amount_total,
-        orderItem: {
-          create: [
-            ...lineItems.map((item) => ({
-              orderId: 100, // TODO: replace orderId and prodId
-              prodId: 100,
-              quantity: item.quantity,
-              total: item.total,
-            })),
-          ],
-        },
       },
     });
 
@@ -76,25 +69,58 @@ const handleDBTransactions = async (
       throw new Error(`Unexpected ${order} from order create request to db`);
 
     if (typeof order === 'object' && 'error' in order) throw order;
+
+    const orderItems = await execSideEffect(tx.orderItem.createMany, {
+      data: lineItems.map((item, i) => {
+        const prod = products[i] as Product;
+        return {
+          orderId: order.id,
+          prodId: prod.id,
+          quantity: item.quantity,
+          total: item.total,
+        };
+      }),
+    });
+
+    if (orderItems === null) throw new Error('Failed to Add Order Items');
   });
 };
 
 const updateDB = async (
   successObject: SuccessObject,
-  lineItems: Item[]
+  lineItems: Item[],
+  stripe: Stripe
 ): Promise<void> => {
   // ATOMIC TRANSACTION: https://www.prisma.io/docs/guides/performance-and-optimization/prisma-client-transactions-guide
-  // TODO: Deduct product quantity from db.
-  // TODO: Create recipe/confirmation of purchase by creating a new row in Order and Order_Items table.
-  const result = await execSideEffectWithError(
+  const result = await execSideEffect(
     handleDBTransactions,
     lineItems,
     successObject
   );
 
   if (result !== undefined) {
-    console.error(result);
-    // TODO: Stripe undo transaction
+    console.error(
+      'Something went wrong with one of the interactions in the DB with stripeWebhook'
+    );
+
+    const session = await stripe.checkout.sessions.retrieve(successObject.id);
+    const paymentIntentId = session.payment_intent;
+
+    const refund = await execSideEffect(stripe.refunds.create, {
+      payment_intent: paymentIntentId,
+    });
+
+    if (refund === null) {
+      console.error('Unknown error when trying to reverse checkout payment.');
+    }
+
+    const cancelledSession = execSideEffect(
+      stripe.checkout.sessions.expire,
+      successObject.id
+    );
+
+    if (cancelledSession === null)
+      console.error('Unknown error when trying to cancel checkout session');
   }
 };
 
@@ -125,18 +151,17 @@ export const stripeWebhook = async (req: Request, res: Response) => {
   }
 
   const successObject = event.data.object as SuccessObject; // Type not defined for object
-  // TODO: Pass userId (customer's id) into successObject...
-  // console.log(successObject.metadata);
+  if (successObject.payment_status !== 'paid') return res.status(202).send();
 
   const result = await stripe.checkout.sessions.listLineItems(successObject.id);
-
   updateDB(
     successObject,
     result.data.map((item) => ({
       name: item.description,
       quantity: item.quantity ?? 0,
       total: item.amount_total,
-    }))
+    })),
+    stripe
   );
 
   return res.status(201).send();
@@ -146,4 +171,21 @@ export const stripeWebhook = async (req: Request, res: Response) => {
 Important Information:
 stripe trigger checkout.session.completed --add "checkout_session:metadata.userId=3"
 https://github.com/stripe/stripe-cli/issues/864
+*/
+
+// orderItem: {
+//   create: [
+//     ...lineItems.map((item) => ({
+//       orderId: 100, // TODO: replace orderId and prodId
+//       prodId: 100,
+//       quantity: item.quantity,
+//       total: item.total,
+//     })),
+//   ],
+// },
+
+/*
+
+stripe trigger checkout.session.completed --add checkout_session:metadata.userId=2
+
 */
