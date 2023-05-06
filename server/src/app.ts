ab5212aeaed75f6c1e536c07a74cb4b13f1bf4a9
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-05-05 21:59:41 -0400

    downgraded passport version

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,9 +25,10 @@ const sessionOptions = {
 };
 
 const app = express();
+console.log(CLIENT_URL);
 
 app.use(maybeExclude(express.json(), ['/api/stripeWebhook']) as any);
-app.use(cors({ origin: CLIENT_URL, credentials: true }));
+// app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use('/api/stripeWebhook', express.raw({ type: '*/*' }));
 app.use(express.json());
 app.use(session(sessionOptions));
