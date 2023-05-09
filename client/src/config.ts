256a2d42576e9ec9894ae83a19ec19ae722ed486
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-05-08 22:33:47 -0400

    fixed cors and added navigate to stripe function and cart count and swapping between username and login span depending on if the user is loggedin in

---
+++ client/src/config.ts
@@ -0,0 +1,4 @@
+export const SERVER_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'localhost:3001'
+    : 'https://drab-pink-dalmatian-gear.cyclic.app';
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import store, {selectUser, selectCart} from '../../store'
+import store, { selectUser, selectCart } from '../../store';
 import productService from '../../services/productService';
 import { setProducts } from '../../reducers/productReducer';
 import { Link } from 'react-router-dom';
@@ -26,20 +26,20 @@ export default function HomePage(): JSX.Element {
 
   return (
     <>
-
       {products.length > 0 && (
-        <div className=" bg-neutral-white w-screen flex flex-row justify-center">
-          <main className="flex gap-x-8 gap-y-5 justify-center flex-wrap max-w-screen-xl mt-6">
+        <div className=" flex w-screen flex-row justify-center bg-neutral-white">
+          <main className="mt-6 flex max-w-screen-xl flex-wrap justify-center gap-x-8 gap-y-5">
             {products.map((product) => (
               <Link
                 className="group"
                 key={product.id}
-                to={`/ProductPage/${product.id}`}
+                to={`/ProductPage/${product.id ?? -1}`}
               >
                 <figure className="w-60">
                   <img
                     className="h-52 w-full border-4 border-neutral-gray"
                     src={product.image ?? ''}
+                    alt="Product"
                   />
                   <div className="flex h-36 flex-col justify-between bg-neutral-gray">
                     <figcaption className="ml-2 group-hover:underline">
